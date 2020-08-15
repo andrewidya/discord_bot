a3ef38dc0bd52ea6fe971c7c3b77c47afb9d2a19
Andry UN <andrywidyaputra@gmail.com>
2020-08-16 01:52:12 +0700

    Fix group command for wepon role & platfor role

--- cogs/game_roles.py
+++ cogs/game_roles.py
@@ -82,72 +82,72 @@ class WeaponRole(BaseRoleManager, name='weapon-utama-hunter'):
         if ctx.invoked_subcommand is None:
             await ctx.send("set-main-weapon memerlukan paramater weapon")
 
-    @commands.command(name="great-sword", aliases=["gs",], case_insensitive=True)
+    @set_weapon.command(name="great-sword", aliases=["gs",], case_insensitive=True)
     async def great_sword(self, ctx):
         """Great Sword"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="long-sword", aliases=["ls",], case_insensitive=True)
+    @set_weapon.command(name="long-sword", aliases=["ls",], case_insensitive=True)
     async def long_sword(self, ctx):
         """Long Sword"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="sword-and-shield", aliases=["sns",], case_insensitive=True)
+    @set_weapon.command(name="sword-and-shield", aliases=["sns",], case_insensitive=True)
     async def sword_and_shield(self, ctx):
         """Sword & Shield"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="dual-blade", aliases=["db",], case_insensitive=True)
+    @set_weapon.command(name="dual-blade", aliases=["db",], case_insensitive=True)
     async def dual_blade(self, ctx):
         """Dual Blade"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="hamer", aliases=["hmr",], case_insensitive=True)
+    @set_weapon.command(name="hamer", aliases=["hmr",], case_insensitive=True)
     async def hammer(self, ctx):
         """Hammer"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="hunting-horn", aliases=["hh",], case_insensitive=True)
+    @set_weapon.command(name="hunting-horn", aliases=["hh",], case_insensitive=True)
     async def hunting_horn(self, ctx):
         """Hunting Horn"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="lance", aliases=["lc",], case_insensitive=True)
+    @set_weapon.command(name="lance", aliases=["lc",], case_insensitive=True)
     async def lance(self, ctx):
         """Lance"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="gunlance", aliases=["gl",], case_insensitive=True)
+    @set_weapon.command(name="gunlance", aliases=["gl",], case_insensitive=True)
     async def gunlance(self, ctx):
         """Gunlance"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="insect-glaive", aliases=["ig",], case_insensitive=True)
+    @set_weapon.command(name="insect-glaive", aliases=["ig",], case_insensitive=True)
     async def insect_glaive(self, ctx):
         """Insect Glaive"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="charge-blade", aliases=["cb",], case_insensitive=True)
+    @set_weapon.command(name="charge-blade", aliases=["cb",], case_insensitive=True)
     async def charge_blade(self, ctx):
         """Charge Blade"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="switch-axe", aliases=["sa",], case_insensitive=True)
+    @set_weapon.command(name="switch-axe", aliases=["sa",], case_insensitive=True)
     async def switch_axe(self, ctx):
         """Switch Axe"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="bow", aliases=["bw",], case_insensitive=True)
+    @set_weapon.command(name="bow", aliases=["bw",], case_insensitive=True)
     async def bow(self, ctx):
         """Bow"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="light-bowgun", aliases=["lbg",], case_insensitive=True)
+    @set_weapon.command(name="light-bowgun", aliases=["lbg",], case_insensitive=True)
     async def light_bowgun(self, ctx):
         """Light Bowgun"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="heavy-bowgun", aliases=["hbg",], case_insensitive=True)
+    @set_weapon.command(name="heavy-bowgun", aliases=["hbg",], case_insensitive=True)
     async def heavy_bowgun(self, ctx):
         """Heavy Bowgun"""
         await self.set_role(ctx, ctx.command.name)
@@ -161,57 +161,57 @@ class GameTitleRole(BaseRoleManager, name='title-series'):
     mudah mencari teman berburu sesuai seri yang dimainkan    
     """
     @commands.group(name="set-title-series")
-    async def set_weapon(self, ctx):
+    async def set_title_series(self, ctx):
         """Untuk set judul seri game yang sedang dimainkan"""
         if ctx.invoked_subcommand is None:
             await ctx.send("set-title-series memerlukan paramater judul seri game")
     
-    @commands.command(name="mhf1", case_insensitive=True)
+    @set_title_series.command(name="mhf1", case_insensitive=True)
     async def mhf1(self, ctx):
         """Monster Hunter Freedom 1"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="mhfu", case_insensitive=True)
+    @set_title_series.command(name="mhfu", case_insensitive=True)
     async def mhfu(self, ctx):
         """Monster Hunter Freedom United"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="mhp3rd", case_insensitive=True)
+    @set_title_series.command(name="mhp3rd", case_insensitive=True)
     async def mhp3rd(self, ctx):
         """Monster Hunter Portable 3rd"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="mh3u", case_insensitive=True)
+    @set_title_series.command(name="mh3u", case_insensitive=True)
     async def mh3u(self, ctx):
         """Monster Hunter 3 Ultimate"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="mh4u", case_insensitive=True)
+    @set_title_series.command(name="mh4u", case_insensitive=True)
     async def mh4u(self, ctx):
         """Monster Hunter 4 Ultimate"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="mhgen", case_insensitive=True)
+    @set_title_series.command(name="mhgen", case_insensitive=True)
     async def mhgen(self, ctx):
         """Monster Hunter Generation"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="mhxx", case_insensitive=True)
+    @set_title_series.command(name="mhxx", case_insensitive=True)
     async def mhxx(self, ctx):
         """Monster Hunter XX"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="mhgu", case_insensitive=True)
+    @set_title_series.command(name="mhgu", case_insensitive=True)
     async def mhgu(self, ctx):
         """Monster Hunter Generation Ultimate"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="mhw", case_insensitive=True)
+    @set_title_series.command(name="mhw", case_insensitive=True)
     async def mhw(self, ctx):
         """Monster Hunter World"""
         await self.set_role(ctx, ctx.command.name)
 
-    @commands.command(name="iceborne", case_insensitive=True)
+    @set_title_series.command(name="iceborne", case_insensitive=True)
     async def mhwi(self, ctx):
         """Monster Hunter World Iceborne"""
         await self.set_role(ctx, ctx.command.name)
