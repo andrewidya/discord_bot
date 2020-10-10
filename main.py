fba1b28f40800bbeecf0ddbbe3c7bc9994640c02
Andry UN <andrywidyaputra@gmail.com>
2020-10-11 01:23:11 +0700

    Redesign set role command

--- main.py
+++ main.py
@@ -15,12 +15,14 @@ EXTENSIONS = [
     'cogs.game_roles'
 ]
 
+
 class HelpCommand(DefaultHelpCommand):
     def __init__(self, **options):
         super().__init__(**options)
 
     def get_ending_note(self):
-        """Returns help command's ending note. This is overrided version on i18n purposes."""
+        """Returns help command's ending note.
+        This is overrided version on i18n purposes."""
         command_name = self.invoked_with
         return "Ketik {0}{1} <command> untuk info lebih lanjut.\n" \
                "Kamu juga bisa ketik {0}{1} <category> untuk info category.".format(
@@ -31,7 +33,8 @@ class HelpCommand(DefaultHelpCommand):
 
     def subcommand_not_found(self, command, string):
         if isinstance(command, discord.core.Group) and len(command.all_commands) > 0:
-            return 'Command "{0.qualified_name}" tidak punya subcommand {1}'.format(command, string)
+            return 'Command "{0.qualified_name}" tidak punya subcommand {1}'.format(
+                        command, string)
         return 'Command "{0.qualified_name}" tidak punya subcommand'.format(command)
 
 
@@ -43,9 +46,11 @@ class GuildMarm(commands.Bot):
                 for channel in guild.channels:
                     if channel.name in ECO[guild.name]["allowed_channel"]:
                         await channel.send(
-                            content="Halo salam hunter!!!, selamat berburu :grinning::clap:")
+                            content="Halo salam hunter!!!, selamat berburu"
+                                    ":grinning::clap:")
                         break
                 break
+
 
 if __name__ == "__main__":
     help_command = HelpCommand(command_attrs={'help': 'Tampilkan pesan ini'})
@@ -53,7 +58,7 @@ if __name__ == "__main__":
         command_prefix=config.COMMAN_PREFIX,
         owner_id=config.OWNER_ID,
         help_command=help_command)
-        
+
     for ext in EXTENSIONS:
         guildmarm.load_extension(ext)
 
