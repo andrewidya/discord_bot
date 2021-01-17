da415703703aa2d7e31477dd198821b96ba70486
Andry UN <andrywidyaputra@gmail.com>
2021-01-17 17:03:36 +0700

    Feat add import export data

--- guildmarm/settings.py
+++ guildmarm/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dashboard'
+    'dashboard',
+    'import_export'
 ]
 
 MIDDLEWARE = [
